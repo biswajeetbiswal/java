6c512fdf7e480fb0cf67e163074e56e98701ba58
biswajeetbiswal <biswajeetbiswal22@gmail.com>
2023-08-17 12:09:51 +0530

    initial commit

---
+++ BigInteger/Basic.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+class Basic
+{
+    public static void main(String args[])
+    {
+        BigInteger x=new BigInteger("1");
+        BigInteger y=new BigInteger("45");
+        x=BigInteger.valueOf(45);
+        System.out.println(x.add(y));
+
+    }
+}
